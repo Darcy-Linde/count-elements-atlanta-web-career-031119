e479c88bb785182927e0142ac7d204b2dc603723
Darcy Linde <lindedarcy@gmail.com>
2019-03-11 21:46:48 +0000

    Automatically backed up by Learn

--- count_elements.rb
+++ count_elements.rb
@@ -1,5 +1,5 @@
 def count_elements(array)
-  array.each_with_object(Hash.new(0)) { |word,counts| counts[word] += 1 }
+  array.each_with_object(Hash.new(0)) { |item,counts| counts[item] += 1 }
 end
 
 end
